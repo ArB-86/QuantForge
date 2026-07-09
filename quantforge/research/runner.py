f27501d4dc414318c0c4cf8828619b4fab57a95f
ArB-86 <YOUR_GITHUB_EMAIL>
2026-07-09 13:14:32 +0200

    refactor: delegate experiment runner to pipeline

--- quantforge/research/runner.py
+++ quantforge/research/runner.py
@@ -1,77 +1,14 @@
-from quantforge.engine.trainer import train
-from quantforge.engine.backtest import backtest
-
-from quantforge.database.logger import (
-    ExperimentLogger,
-)
-
-from quantforge.optimization.objectives import (
-    portfolio_objective,
-)
-
-from quantforge.research.validator import (
-    ExperimentValidator,
-)
+from quantforge.pipeline.experiment_pipeline import ExperimentPipeline
 
 
 class ExperimentRunner:
 
     def __init__(self):
 
-        self.logger = ExperimentLogger()
-
-        self.validator = ExperimentValidator()
+        self.pipeline = ExperimentPipeline()
 
     def __call__(self, config):
 
-        #
-        # Train
-        #
+        context = self.pipeline.run(config)
 
-        train(config)
-
-        #
-        # Backtest
-        #
-
-        _, metrics = backtest(config)
-
-        print("\nTRIAL METRICS")
-        for k, v in metrics.items():
-            print(k, "=", v)
-        print()
-
-        #
-        # Validate
-        #
-
-        valid, reasons = self.validator.validate(
-            metrics
-        )
-
-        metrics["Valid"] = valid
-        metrics["RejectReason"] = "; ".join(
-            reasons
-        )
-
-        score = portfolio_objective(metrics)
-
-        if not valid:
-            score -= 100
-
-        metrics["Score"] = score
-
-        self.logger.log(
-            config,
-            metrics,
-            score,
-        )
-
-        if not valid:
-
-            print("=" * 80)
-            print("EXPERIMENT REJECTED")
-            print(metrics["RejectReason"])
-            print("=" * 80)
-
-        return metrics
+        return context.metrics
