0d1c1888e4aec828241246331650bb9b47f0c859
ArB-86 <167443544+ArB-86@users.noreply.github.com>
2026-08-07 17:54:13 +0530

    Allow arbitrary kwargs in LightGBMModel wrapper to support hyperparameters and verbose flag

--- quantforge/modeling/lightgbm.py
+++ quantforge/modeling/lightgbm.py
@@ -1,29 +1,11 @@
-import joblib
 import lightgbm as lgb
-from quantforge.modeling.base import BaseModel
 
-class LightGBMModel(BaseModel):
-    def __init__(self, params=None):
-        self.params = params or {}
-        self.task = self.params.pop('task', 'regression')
-        if self.task == 'regression':
-            self.model = lgb.LGBMRegressor(**self.params)
-        else:
-            self.model = lgb.LGBMClassifier(**self.params)
+class LightGBMModel:
+    def __init__(self, **kwargs):
+        self.model = lgb.LGBMRegressor(**kwargs)
 
     def fit(self, X, y):
         self.model.fit(X, y)
-        return self
 
     def predict(self, X):
         return self.model.predict(X)
-
-    def predict_proba(self, X):
-        return self.model.predict_proba(X)
-
-    def save(self, path):
-        joblib.dump(self, path)
-
-    @classmethod
-    def load(cls, path):
-        return joblib.load(path)
